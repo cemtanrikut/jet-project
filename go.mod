7d52792e342953f30641de2b1c1c9b7eded992cf
Cem Tanrikut <cemtanrikut@gmail.com>
2024-12-16 18:41:37 +0100

    initialize provider and content

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main.go
+
+go 1.22.1
+
+require github.com/cemtanrikut/jet-project v0.0.0-20241216173804-024097f2fe90
--- main.go
+++ main.go
@@ -1,5 +1,50 @@
-package jetproject
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/cemtanrikut/jet-project/token"
+
+	"github.com/cemtanrikut/jet-project/storage"
+)
 
 func main() {
+	// Initialize Token Provider
+	tokenProvider := &token.SalesforceTokenProvider{
+		ClientID:     "client-id",
+		ClientSecret: "client-secret",
+		AuthURL:      "https://auth.salesforce.com/token",
+	}
 
+	accessToken, err := tokenProvider.GetAccessToken()
+	if err != nil {
+		log.Fatalf("Failed to get access token: %v", err)
+	}
+
+	fmt.Printf("Access Token: %s\n", accessToken)
+
+	// Select Storage Strategy
+	var storageStrategy storage.StorageStrategy
+	storageType := "local" // Can be "local", "s3", "gcs"
+
+	switch storageType {
+	case "local":
+		storageStrategy = &storage.LocalStorageStrategy{}
+	case "s3":
+		storageStrategy = &storage.S3StorageStrategy{}
+	case "gcs":
+		storageStrategy = &storage.GCSStorageStrategy{}
+	default:
+		log.Fatalf("Invalid storage type: %s", storageType)
+	}
+
+	// Save Content
+	content := `{"example": "This is a test content block"}`
+	err = storageStrategy.SaveContent(content, "output.json")
+	if err != nil {
+		log.Fatalf("Failed to save content: %v", err)
+	}
+
+	fmt.Println("Content saved successfully!")
 }
