bf559ffd91ae91293aff3cce5cfedb8e76c2a719
Cem Tanrikut <cemtanrikut@gmail.com>
2024-12-16 18:32:54 +0100

    add strategy pattern

--- storage/strategy.go
+++ storage/strategy.go
@@ -1,1 +1,6 @@
 package storage
+
+// StorageStrategy defines the interface for different storage strategies.
+type StorageStrategy interface {
+	SaveContent(content string, fileName string) error
+}
